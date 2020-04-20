774c621aee1bba88d6b226bf7390c61b022859d2
manish-sage <manish.kc@joinsage.com>
2020-04-20 22:36:04 +0545

    feat: remove and increase item in the cart

--- books.js
+++ books.js
@@ -181,6 +181,22 @@ class DisplayProducts {
     clearCartBtn.addEventListener("click", () => {
       this.clearCart();
     });
+    cartContent.addEventListener("click", (event) => {
+      if (event.target.classList.contains("remove-item")) {
+        let removeItem = event.target;
+        let id = removeItem.dataset.id;
+        cartContent.removeChild(removeItem.parentElement.parentElement);
+        this.removeItem(id);
+      } else if (event.target.classList.contains("fa-chevron-up")) {
+        let addAmount = event.target;
+        let id = addAmount.dataset.id;
+        let tempItem = cart.find((item) => item.id === id);
+        tempItem.amount = tempItem.amount + 1;
+        Storage.saveCart(cart);
+        this.setCartValue(cart);
+        addAmount.nextElementSibling.innerText = tempItem.amount;
+      }
+    });
   }
 }
 
